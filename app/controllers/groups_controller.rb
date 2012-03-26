43e943fa49f405dd98a241cd33b84d5ec16b2a53
Tim Morgan <tim@timmorgan.org>
2012-03-25 21:26:56 -0500

    Rough start of group functionality.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,14 @@
 class GroupsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource # sets @group
+
+  respond_to :html, :js
+
+  def index
+    @groups = Group.select('*, (select count(*) from roles where group_id=groups.id) as profile_count').all
+  end
+
+  def show
+    @roles = @group.roles.includes(:profile).order("case when roles.name is null then 'z' else 'a' end, profiles.name")
+  end
 end
