9be5d0fca5237115fd52a50cd0221c22f32ae37b
Tim Morgan <tim@timmorgan.org>
2013-04-07 16:32:03 -0500

    Set fallback locale.

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Profiles
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = 'en-US'
+    config.i18n.fallbacks = [:en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
