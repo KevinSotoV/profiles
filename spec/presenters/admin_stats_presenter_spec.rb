e60c2fc7360f57ddc144c87e497f50bc6f5ed0d5
Tim Morgan <tim@timmorgan.org>
2013-04-08 19:26:13 -0500

    Use time zone to create date.

--- spec/presenters/admin_stats_presenter_spec.rb
+++ spec/presenters/admin_stats_presenter_spec.rb
@@ -10,7 +10,10 @@ describe AdminStatsPresenter do
     it 'returns a hash with dates as keys and counts as values' do
       counts = subject.sign_ups_by_date
       expect(counts.keys.map(&:to_s)).to have(30).dates
-      expect(counts.keys[-2..-1]).to eq([Date.today-1, Date.today])
+      expect(counts.keys[-2..-1]).to eq([
+        (Time.zone.now-1.day).to_date,
+        Time.zone.now.to_date
+      ])
       expect(counts.values[-2..-1]).to eq([[], [@profile]])
     end
   end
